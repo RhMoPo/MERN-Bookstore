f471379f9e97f3aff55f3feed7900819ff13bcf2
Rhys Postans <rmp71171@icloud.com>
2024-05-02 18:37:59 +0100

    connected to mongoDB

--- backend/config.js
+++ backend/config.js
@@ -1,1 +1,4 @@
 export const PORT = 5555;
+
+export const mongoDBURL =
+    'mongodb+srv://rmp71171:Rhys4321@mern.cpg3jtr.mongodb.net/?retryWrites=true&w=majority&appName=MERN'
--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,24 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express()
 
-app.listen(PORT, () => {
+
+
+app.get("/", (req, res) => {
+    console.log(req)
+    return res.status(234).send("Hello World")
+});
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log("Connected to MongoDB");
+        app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
+    })
+    .catch((err) => {
+        console.log(err);
+    });
