627075e8eaf3df24d099d38ae7239ba2a70b3c4e
Rhys Postans <rmp71171@icloud.com>
2024-05-04 16:59:57 +0100

    midway through react set up

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,25 @@ import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 import { Book } from "./models/bookModel.js";
 import booksRoute from './routes/booksRoute.js'
+import cors from 'cors'
 
 const app = express();
 
 //middleware for parsing req body
 app.use(express.json());
+
+//middleware for handling cors policy 
+// 1: allow all origins with a default of cors(*)
+app.use(cors())
+//2: allow custom origins 
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type'],
+}))
+
+
+
 
 app.get("/", (req, res) => {
   console.log(req);
